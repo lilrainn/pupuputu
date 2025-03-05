518fcfd8e31d35ebb410ba8302fa8e1bc3cb8e30
egor.borisov <raskrytka055@gmail.com>
2025-03-05 14:08:09 +0300

    Добавили маленьких пузырьков

--- pupuputu.py
+++ pupuputu.py
@@ -1,9 +1,10 @@
 import sys
 
 def bubble_sort(arr):
+    """Функция для сортировки списка методом пузырька."""
     n = len(arr)
-    for i in range(n):
-        for j in range(0, n - i - 1):
+    for i in range(n - 1):
+        for j in range(n - i - 1):
             if arr[j] > arr[j + 1]:
                 arr[j], arr[j + 1] = arr[j + 1], arr[j]
     return arr
